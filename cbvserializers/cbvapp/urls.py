9308f9195c05a0927f8cbf12603ef4fe06830018
goel <jinkiller69@gmail.com>
2020-12-26 11:55:06 +0530

    viewsets added

--- cbvserializers/cbvapp/urls.py
+++ cbvserializers/cbvapp/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
-from . views import StudentList, StudentDetail
+from django.urls import path,include
+from . views import StudentViewSet
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('students',StudentViewSet)
+
+urlpatterns = [
+    path('',include(router.urls))
+]
+
+'''
 urlpatterns = [
     path('',StudentList.as_view(),name='students-list'),
     path('student/<int:pk>/',StudentDetail.as_view(),name='student-detail'),
 ]
+'''
--- cbvserializers/cbvapp/views.py
+++ cbvserializers/cbvapp/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from django.http import Http404
 from rest_framework import generics,mixins
+from rest_framework import viewsets
 # Create your views here.
 '''
 class StudentList(APIView):
@@ -73,11 +74,16 @@ class StudentDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.Des
     def delete(self,request,pk):
         return self.destroy(request,pk)
 '''
-
+'''
 class StudentList(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+'''
+
+class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
