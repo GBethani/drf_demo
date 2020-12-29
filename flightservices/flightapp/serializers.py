aec1d064c2c6c3c4abcd4c7b5bbaf82de41b24e4
goel <jinkiller69@gmail.com>
2020-12-29 10:39:03 +0530

    custom rest api validations added

--- flightservices/flightapp/serializers.py
+++ flightservices/flightapp/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from .models import Flight, Passenger, Reservation
+import re
 
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
+
+    def validate_flightnumber(self,flightnumber):
+        if(re.match("^[a-zA-Z0-9]*$",flightnumber)==None):
+            raise serializers.ValidationError("Only AlphaNumerics are allowed")
+        return flightnumber
 
 class PassengerSerializer(serializers.ModelSerializer):
     class Meta:
