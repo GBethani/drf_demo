6b5fe97304c2bed39ff8a1725b1b11d6c4532bac
goel <jinkiller69@gmail.com>
2020-12-30 10:52:39 +0530

    token authorization added

--- flightservices/flightapp/urls.py
+++ flightservices/flightapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from . views import FlightViewSet, PassengerViewSet, ReservationViewSet, find_flights,save_reservation
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register('flights',FlightViewSet)
@@ -11,4 +12,5 @@ urlpatterns = [
     path('',include(router.urls)),
     path('find_flights/',find_flights,name = 'find-flights'),
     path('save_reservation/',save_reservation,name = 'save-reservation'),
+    path('api-token/',obtain_auth_token,name = 'token-auth'),
 ]
